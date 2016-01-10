477b544fef7732a5d02a42f4a1d776ebc0cde380
Angela Kwon <kwon.angela@gmail.com>
2016-01-10 18:30:26 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+
+var htmlmin = require('gulp-htmlmin');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+// Minify index
+gulp.task('minify', function() {
+	return gulp.src('site/index.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('dist/'))
+});
+
+// js build task
+gulp.task('scripts', function() {
+	return browserify('site/js/app.js')
+		.bundle()
+		.pipe(source('site/app.js'))
+		.pipe(buffer())
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+// concat css
+gulp.task('styles', function() {
+	return gulp.src('site/css/*.css')
+		.pipe(concat('styles.css'))
+		.pipe(gulp.dest('dist/css'));
+});
+
+// build
+gulp.task('build', [minify, scripts, styles]);
